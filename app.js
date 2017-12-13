52640a9dc588c5dc1ce2880dc36d713c6fba799a
jugbeep <patrickbotkins@gmail.com>
2017-12-13 14:40:44 -0700

    bubbles on page, spacing not quite working right

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	console.log('doc ready')
 })
 
+let boardWidth = $('#board').width();
 let bubbleArray = [];
+let bubbleLoop = 1;
 
-function Bubble (name, image, value) {
+function Bubbles (name, image, value) {
 	this.name = name;
 	this.image = image;
 	this.value = value;
@@ -14,48 +16,30 @@ function Bubble (name, image, value) {
 	this.attach = function(){
 		bubbleArray.push(this);
 	};
-	this.attach();
+	for (i = 1; i <= bubbleLoop; i++) {
+		this.attach();
+	};
 }
 
-var bubble1 = new Bubble ("bubble1", "<img id='bubble1' src=bubb3.gif_c200>", 20);
-var bubble2 = new Bubble ('bubble2', "<img id='bubble2' src=crazyBub.gif>", 10);
-var bubble3 = new Bubble ('bubble3', "<img id='bubble3' src=crazyBub1.gif>", 30);
+let bubble1 = new Bubbles ("bubble1", "<img class='bubble' id='bubble1' src=bubb3.gif_c200>", 20);
+let bubble2 = new Bubbles ('bubble2', "<img class='bubble' id='bubble2' src=crazyBub.gif>", 10);
+let bubble3 = new Bubbles ('bubble3', "<img class='bubble' id='bubble3' src=crazyBub1.gif>", 30);
 
 bubbleArray.forEach(function(obj){
 	obj.create();
 });
 
+function spaceRandom(bub){
+	let leftSpacing = Math.floor(Math.random() * boardWidth +1);
+	$('.bubble').css({"margin-left": +leftSpacing+"px"})
+};
+spaceRandom();
 
 
-let randomNumber = Math.floor(Math.random() * 3 + 1) * 10;
 
 
 
-$(function(){
 
-	var thingArray = [];
-
-	function Thing(name){
-  	this.name = name;
-    this.speak = function(){
-    	$('.log').append('<p>Hi my name is ' + this.name + '</p>');
-    };
-    this.attach = function(){
-    	thingArray.push(this);
-    };
-    this.attach();
-  }
-  
-  var mike = new Thing("Mike");
-  var dave = new Thing("Dave");
-  var pete = new Thing("Pete");
-  var john = new Thing("John");
-  
-  thingArray.forEach(function(obj){
-  	obj.speak();
-  });
-
-});
 
 
 
