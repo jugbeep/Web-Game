6e514475c83051dfa378fbb86352a7f54377a3a6
jugbeep <patrickbotkins@gmail.com>
2017-12-13 12:09:08 -0700

    kind of figured out array of objects

--- app.js
+++ app.js
@@ -2,56 +2,60 @@ $(document).ready(function(){
 	console.log('doc ready')
 })
 
+let bubbleArray = [];
+
 function Bubble (name, image, value) {
 	this.name = name;
 	this.image = image;
 	this.value = value;
+	this.create = function (){
+		$('#board').append(this.image);
+	};
+	this.attach = function(){
+		bubbleArray.push(this);
+	};
+	this.attach();
 }
-let bubble1 = new Bubble ("bubble1", "bubb3.gif_c200", 20);
-let bubble2 = new Bubble ('bubble2', 'crazyBub.gif', 10);
-let bubble3 = new Bubble ('bubble3', 'crazyBub1.gif', 30);
 
-Bubble();
-console.log(Bubble)
+var bubble1 = new Bubble ("bubble1", "<img id='bubble1' src=bubb3.gif_c200>", 20);
+var bubble2 = new Bubble ('bubble2', "<img id='bubble2' src=crazyBub.gif>", 10);
+var bubble3 = new Bubble ('bubble3', "<img id='bubble3' src=crazyBub1.gif>", 30);
 
-
-
-
+bubbleArray.forEach(function(obj){
+	obj.create();
+});
 
 
 
+let randomNumber = Math.floor(Math.random() * 3 + 1) * 10;
 
 
 
+$(function(){
+
+	var thingArray = [];
+
+	function Thing(name){
+  	this.name = name;
+    this.speak = function(){
+    	$('.log').append('<p>Hi my name is ' + this.name + '</p>');
+    };
+    this.attach = function(){
+    	thingArray.push(this);
+    };
+    this.attach();
+  }
+  
+  var mike = new Thing("Mike");
+  var dave = new Thing("Dave");
+  var pete = new Thing("Pete");
+  var john = new Thing("John");
+  
+  thingArray.forEach(function(obj){
+  	obj.speak();
+  });
+
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-// function Bubble(image, value) {
-// 	this.image: image
-// 	this.value: 1
-// }
-
-// Bubble.prototype.create = function (){
-	
-// 	let container = $('#canvas');
-// 	let image = append('img');
-// 	img.src = this.src;
-// 	img.className = 'bubble';
-// 	img.style.width = '200px';
-// 	container.append(img);
-// }
-
-// function bubbleGo(){
-// 	$('#canvas').append.Bubble.image;
-// }
 
